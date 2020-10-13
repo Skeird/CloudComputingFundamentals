1823456236e37dceeaf4fe62b2eade31b95ec126
Vincent LANDAIS <vincent.landais@epitech.eu>
2020-10-13 11:54:33 +0200

    new build, added a component that makes un GET request

---
+++ build/precache-manifest.31cd686c37810958618995002045762a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc8eec54ab0bec47c8add0aa3bfddcfc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2259647f498adbc60d39",
+    "url": "/static/css/main.e4d530f7.chunk.css"
+  },
+  {
+    "revision": "3804f09068b0956ffe5f",
+    "url": "/static/js/2.39d453e9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.39d453e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2259647f498adbc60d39",
+    "url": "/static/js/main.7cd45407.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
