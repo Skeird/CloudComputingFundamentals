c065ac12b0ff115b071b6a556db994e457fdc5d6
Vincent LANDAIS <vincent.landais@epitech.eu>
2020-11-03 11:31:05 +0100

    wrong url fixed

--- webapp/src/components/test_api/test_api.js
+++ webapp/src/components/test_api/test_api.js
@@ -16,7 +16,7 @@ export default class Test_api extends React.Component {
             headers: { 
                 'Content-Type': 'application/json'
             },
-            url: 'https://1m0whmainf.execute-api.us-east-1.amazonaws.com/test-stage/test-resource?foo=test-de-call-api!',
+            url: 'https://k9wwepp62a.execute-api.us-east-1.amazonaws.com/Prod/hello?foo=bar',
         }).then(response => {
             this.setState({
                 test: response.data .foo
